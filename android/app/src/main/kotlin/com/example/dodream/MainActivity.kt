ba3e75b09d563b43f79609bae6bae3e03968eb98
jaypyon <scorpion@dgu.ac.kr>
2021-11-29 13:36:42 +0900

    chore: manual approve

--- android/app/src/main/kotlin/com/example/dodream/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/dodream/MainActivity.kt
@@ -1,6 +1,20 @@
 package com.example.dodream
 
 import io.flutter.embedding.android.FlutterActivity
+import android.content.Intent.getIntent
+
+import android.os.Bundle
+
+
+
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        // use software rendering (ideally only when you need to)
+       getIntent().putExtra("enable-software-rendering", true)
+
+        // start Flutter
+        super.onCreate(savedInstanceState)
+    }
 }
